59fa7f25e05c2b7b2cc8bc7606b6cfb4a596b6ad
Sadeq N. Yazdi <code@sadeq.uk>
2023-10-02 11:58:36 +0100

    minor improvement for deployment script
    Signed-off-by: Sadeq N. Yazdi <code@sadeq.uk>

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,8 @@ SERVICE_NAME="website@$APP_DEPLOY_NAME"
 go get -u
 go build -ldflags "-w -s" -o "$APP_NAME" && echo "Build completed." && \
 upx "$APP_NAME" && echo "Compress app completed." && \
-ssh "$SERVER" "systemctl stop '$SERVICE_NAME'; [ -f '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' ] && mv '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app.old' && echo 'Backup old app completed'; exit 0"
-scp "$APP_NAME" "$SERVER:/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app"  && echo 'copy new service app file completed' && \
-ssh "$SERVER" "systemctl daemon-reload; systemctl enable '${SERVICE_NAME}' ; systemctl start '${SERVICE_NAME}'" && echo 'start service completed' && \
+ssh $SERVER_SSH_PARAM "$SERVER" "systemctl stop '$SERVICE_NAME'; [ -f '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' ] && mv '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app.old' && echo 'Backup old app completed'; exit 0"
+scp $SERVER_SCP_PARAM "$APP_NAME" "$SERVER:/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app"  && echo 'copy new service app file completed' && \
+ssh $SERVER_SSH_PARAM "$SERVER" "systemctl daemon-reload; systemctl enable '${SERVICE_NAME}' ; systemctl start '${SERVICE_NAME}'" && echo 'start service completed' && \
 echo -e "${BGREEN}Deploy successfully done${NC}" || echo -e "${BRED}Deploy failed${NC}"
 
