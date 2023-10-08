7346229557309f51ff072ed564195e7b014e196b
Sadeq N. Yazdi <code@sadeq.uk>
2023-10-08 15:39:46 +0100

    update used packages
    Signed-off-by: Sadeq N. Yazdi <code@sadeq.uk>

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # DEFAULT SETTINGS
-SERVER="user@example.sadeq.uk"
+SERVER="user@service.example.com"
 SERVER_SSH_PARAM=""
 SERVER_SCP_PARAM=""
-APP_DEPLOY_NAME="example.sadeq.uk"
+APP_DEPLOY_NAME="service.example.com"
 
 
 [ -f "$HOME/.colors.sh" ] && . "$HOME/.colors.sh"
@@ -22,7 +22,7 @@ SERVICE_NAME="website@$APP_DEPLOY_NAME"
 go get -u
 go build -ldflags "-w -s" -o "$APP_NAME" && echo "Build completed." && \
 upx "$APP_NAME" && echo "Compress app completed." && \
-ssh $SERVER_SSH_PARAM "$SERVER" "systemctl stop '$SERVICE_NAME'; [ -f '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' ] && mv '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app.old' && echo 'Backup old app completed'; exit 0"
+ssh $SERVER_SSH_PARAM "$SERVER" "systemctl stop '$SERVICE_NAME'; mkdir -p '/opt/websites/${APP_DEPLOY_NAME}'; [ -f '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' ] && mv '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app' '/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app.old' && echo 'Backup old app completed'; exit 0"
 scp $SERVER_SCP_PARAM "$APP_NAME" "$SERVER:/opt/websites/${APP_DEPLOY_NAME}/${APP_DEPLOY_NAME}.app"  && echo 'copy new service app file completed' && \
 ssh $SERVER_SSH_PARAM "$SERVER" "systemctl daemon-reload; systemctl enable '${SERVICE_NAME}' ; systemctl start '${SERVICE_NAME}'" && echo 'start service completed' && \
 echo -e "${BGREEN}Deploy successfully done${NC}" || echo -e "${BRED}Deploy failed${NC}"
